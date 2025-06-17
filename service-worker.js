c0ee6dd26728a9736185b584f5d0b2bdcf999b77
JocaNBG <ddoslo@gmail.com>
2025-06-17 11:31:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,38 @@
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("turnir-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+const CACHE_VERSION = "v2"; // menjaj pri svakom većem update-u
+const CACHE_NAME = `turnir-cache-${CACHE_VERSION}`;
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
